90b1089119c672b7d14f6635945b58bcac1661b3
Russell Waite <russellwaite@gmail.com>
2022-12-02 07:01:02 +0000

    updated day2 so it runs faster

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -13,110 +13,60 @@ fn main() {
 }
 
 fn day2_1_result(path: &str) -> u64 {
-    let cals = read_file(path);
-    cals.iter().map(|(them, us)| calculate_score(them, us)).sum()
+    calculate_score_based_on_strategy(path, true)
 }
 
 fn day2_2_result(path: &str) -> u64 { 
-    let cals = read_file(path);
-    cals.iter().map(|(them, us)| calculate_score_pt2(them, us)).sum()
+    calculate_score_based_on_strategy(path, false)
 }
 
-// this is quite verbose...
-fn calculate_score(opponent: &str, choice: &str) -> u64 {
-// 1 for X, 2 for Y, 3 for Z
-// 0 for lose, 3 draw, 6 win
-    match opponent {
-        "A" => {
-            match choice {
-                "X" => 4, // 1 + draw 3 
-                "Y" => 8, // 2 + win 6
-                "Z" => 3, // 3 + loss 0
-                _ => panic!("invaid choice")
+fn calculate_score_based_on_strategy(path: &str, x_is_choice: bool) -> u64 {
+    let data = read_file(path);
+    
+    let result = data.lines()
+        .map(|line| {
+            match line {
+                "A X" => (4,3),
+                "A Y" => (8,4),
+                "A Z" => (3,8),
+                "B X" => (1,1),
+                "B Y" => (5,5),
+                "B Z" => (9,9),
+                "C X" => (7,2),
+                "C Y" => (2,6),
+                "C Z" => (6,7),
+                _ => panic!("whatever you've read in - it's BAD...")
             }
-        },
-        "B" => {
-            match choice {
-                "X" => 1, // 1 + loss 0
-                "Y" => 5, // 2 + draw 3
-                "Z" => 9, // 3 + win 6
-                _ => panic!("invaid choice")
-            }
-        },
-        "C" => { 
-            match choice {
-                "X" => 7, // 1 + win 6
-                "Y" => 2, // 2 + loss 0
-                "Z" => 6, // 3 + draw 3
-                _ => panic!("invaid choice")
-            }
-        },
-        _ => panic!("that's not a valid opponent value")    
-    }
+        })
+        .map(|(choice, strategy)| if x_is_choice { choice } else { strategy });
+
+    result.sum()
 }
 
-// copying this is nasty as it's so much boilerplate
-fn calculate_score_pt2(opponent: &str, outcome: &str) -> u64 {
-// x lose, y draw, z win
-    match opponent {
-        "A" => {
-            match outcome {
-                "X" => 3, // 0 loss as we pick scissors 3 
-                "Y" => 4, // 3 draw as we pick rock 1
-                "Z" => 8, // 6 win as we pick paper 2
-                _ => panic!("invaid outcome")
-            }
-        },
-        "B" => {
-            match outcome {
-                "X" => 1, // 0 loss, rock 1
-                "Y" => 5, // 3 draw,paper 2 
-                "Z" => 9, // 6 win, scissors 3
-                _ => panic!("invaid outcome")
-            }
-        },
-        "C" => { 
-            match outcome {
-                "X" => 2, // 0 loss, paper 2
-                "Y" => 6, // 3 draw, scissors 3
-                "Z" => 7, // 6 win, rock 1
-                _ => panic!("invaid outcome")
-            }
-        },
-        _ => panic!("that's not a valid opponent value")    
-    }
-}
-
-fn read_file(path: &str) -> Vec<(String, String)> {
-    let contents = std::fs::read_to_string(path)
-        .unwrap_or_else(|_| panic!("couldn't open input file: {}", path));
-
-    let result: Vec<(String, String)> = contents.lines()
-        .map(|line| 
-                {
-                    // let line = line.unwrap();
-                    let temp:Vec<&str> = line.split(' ').collect();
-                    ((temp[0]).to_owned(), (temp[1]).to_owned())
-                })
-        .collect()
-    ;
-    result
+fn read_file(path: &str) -> String {
+    std::fs::read_to_string(path)
+        .unwrap_or_else(|_| panic!("couldn't open input file: {}", path))
 }
 
 #[test]
 fn read_file_test() {
-    let cals = read_file("test_input");
-    assert_eq!(cals.len(), 3);
+    let rawdata = read_file("test_input");
+    let mut lines = rawdata.lines();
+    assert_eq!(lines.next(), Some("A Y"));
+    assert_eq!(lines.next(), Some("B X"));
+    assert_eq!(lines.next(), Some("C Z"));
 }
 
 #[test]
 fn day2_1_result_1_test() {
     assert_eq!(day2_1_result("test_input"), 15);
+    
     assert_eq!(day2_1_result("input"), 9651);
 }
 
 #[test]
 fn day2_2_result_1_test() {
     assert_eq!(day2_2_result("test_input"), 12);
+
     assert_eq!(day2_2_result("input"), 10560); 
 }
