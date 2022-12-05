d7b79817022dcbc2924b1ce9266db585ce1ca7ab
Russell Waite <russellwaite@gmail.com>
2022-12-05 13:15:21 +0000

    rustfmt

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,9 +1,10 @@
 pub fn day5_1_result(path: &str) -> String {
-    let (stack, instructions)  = read_file(path);
+    let (stack, instructions) = read_file(path);
     let mut stacks = create_stacks(stack);
 
-    instructions.iter()
-        .map(|i| (i[0],i[1], i[2]))
+    instructions
+        .iter()
+        .map(|i| (i[0], i[1], i[2]))
         .for_each(|(count, from, to)| {
             (0..count).for_each(|_| {
                 if let Some(a) = stacks[from as usize - 1].pop() {
@@ -12,35 +13,41 @@ pub fn day5_1_result(path: &str) -> String {
             });
         });
 
-    stacks.iter().map(|stack| *stack.last().unwrap_or(&32u8) as char).collect() 
+    stacks
+        .iter()
+        .map(|stack| *stack.last().unwrap_or(&32u8) as char)
+        .collect()
 }
 
 pub fn day5_2_result(path: &str) -> String {
-    let (stack, instructions)  = read_file(path);
+    let (stack, instructions) = read_file(path);
 
     let mut stacks = create_stacks(stack);
 
-    instructions.iter()
-        .map(|i| (i[0],i[1], i[2]))
+    instructions
+        .iter()
+        .map(|i| (i[0], i[1], i[2]))
         .for_each(|(count, from, to)| {
             let index = stacks[from as usize - 1].len() - count as usize;
             let mut movement = stacks[from as usize - 1].drain(index..).collect();
             stacks[to as usize - 1].append(&mut movement);
         });
 
-    stacks.iter().map(|stack| *stack.last().unwrap() as char).collect()
+    stacks
+        .iter()
+        .map(|stack| *stack.last().unwrap() as char)
+        .collect()
 }
 
 fn create_stacks(data: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
-    let num_of_stacks = 1 + data.first().unwrap().len()/4;
-    let mut transposed: Vec<Vec<u8>> = vec![vec![];num_of_stacks];
+    let num_of_stacks = 1 + data.first().unwrap().len() / 4;
+    let mut transposed: Vec<Vec<u8>> = vec![vec![]; num_of_stacks];
     data.iter()
         .rev()
-        .skip(1) // the line with index numbers   
-        .flat_map(|bytes|
-            bytes.chunks(4).map(|quad| quad[1]).collect::<Vec<u8>>())
+        .skip(1) // the line with index numbers
+        .flat_map(|bytes| bytes.chunks(4).map(|quad| quad[1]).collect::<Vec<u8>>())
         .enumerate()
-        .for_each(|(index,char)| {
+        .for_each(|(index, char)| {
             if char > 64u8 && char < 91u8 {
                 transposed[index % num_of_stacks].push(char);
             }
@@ -50,24 +57,21 @@ fn create_stacks(data: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
 }
 
 fn read_file(path: &str) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
-    
     let raw_data = std::fs::read_to_string(path)
-        .unwrap_or_else(
-            |_| panic!("couldn't open input file: {}", path));    
-    
-    let state = raw_data.lines()
-            .take_while(|line| !line.is_empty())
-            .map(|line| line.as_bytes().to_vec())
-            .collect::<Vec<Vec<u8>>>();
+        .unwrap_or_else(|_| panic!("couldn't open input file: {}", path));
 
-    let instructions = raw_data.lines()
-            .skip_while(|line| !line.starts_with("move "))
-            .map(|line| line.split(' '))
-            .map(|words| words.filter_map(
-                    |word| word.parse::<u8>().ok()
-                    ).collect()
-                )
-            .collect::<Vec<Vec<u8>>>();
+    let state = raw_data
+        .lines()
+        .take_while(|line| !line.is_empty())
+        .map(|line| line.as_bytes().to_vec())
+        .collect::<Vec<Vec<u8>>>();
+
+    let instructions = raw_data
+        .lines()
+        .skip_while(|line| !line.starts_with("move "))
+        .map(|line| line.split(' '))
+        .map(|words| words.filter_map(|word| word.parse::<u8>().ok()).collect())
+        .collect::<Vec<Vec<u8>>>();
 
     (state, instructions)
 }
@@ -88,6 +92,6 @@ fn day5_1_result_1_test() {
 #[test]
 fn day5_2_result_1_test() {
     assert_eq!(day5_2_result("test_input"), "MCD");
-    
+
     assert_eq!(day5_2_result("input"), "LVZPSTTCZ");
 }
